d28178ae33ba9edd719f3edaad5b6c3ee17f7d98
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-03-13 20:05:36 +0000

    Initial Commit

---
+++ MusicByLocationV3/MusicByLocationV3App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationV3App.swift
+//  MusicByLocationV3
+//
+//  Created by Perkins, Alexander (ABH) on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicByLocationV3UITests/MusicByLocationV3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MusicByLocationV3UITestsLaunchTests.swift
+//  MusicByLocationV3UITests
+//
+//  Created by Perkins, Alexander (ABH) on 13/03/2023.
+//
+
+import XCTest
+
+class MusicByLocationV3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
